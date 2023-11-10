f2c5dfccd50f96b70ef675dab12179c55f7586a4
Codewith-Mhd <150380941+Codewith-Mhd@users.noreply.github.com>
2023-11-10 17:11:36 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Login from "./Components/Login/Login";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+const Login = () => {
+  return (
+    <div className="login-form">
+      <h1>Sign In</h1>
+      <input type="text" className="login-input" placeholder="User Name..." />
+      <input type="password" placeholder="Password" className="login-input" />
+      <button type="submit" className="login-btn">
+        Sign In
+      </button>
+    </div>
+  );
+};
+
+export default Login;
